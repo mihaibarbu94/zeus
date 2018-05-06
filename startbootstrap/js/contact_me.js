8e544bf36db71aab0a02762b1b59fe1094405d0c
Mihai  Barbu <mihai.barbu14@imperial.ac.uk>
2018-05-06 22:13:41 +0100

    mended security issues reagarding hoek

--- startbootstrap/js/contact_me.js
+++ startbootstrap/js/contact_me.js
@@ -73,11 +73,3 @@ $(function() {
 $('#name').focus(function() {
   $('#success').html('');
 });
-
-$(function() {
-    $('input[type=file]').change(function(){
-        var t = $(this).val();
-        var labelText = 'File : ' + t.substr(12, t.length);
-        $(this).prev('label').text(labelText);
-    })
-});
