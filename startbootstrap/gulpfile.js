fe591ae5b10090200b7d9488f166c7892fd62ee3
Mihai  Barbu <mihai.barbu14@imperial.ac.uk>
2018-05-06 22:53:36 +0100

    mended security issues reagarding hoek + added browser sync

--- startbootstrap/gulpfile.js
+++ startbootstrap/gulpfile.js
@@ -127,3 +127,24 @@ gulp.task('dev', ['css', 'js', 'browserSync'], function() {
   gulp.watch('./js/*.js', ['js']);
   gulp.watch('./*.html', browserSync.reload);
 });
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("./scss/*.scss", ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("./scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("./css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
