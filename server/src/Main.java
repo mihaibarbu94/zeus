8268cce4ac7e5b6372e4f72e449dc639116cdd42
Mihai  Barbu <mihai.barbu14@imperial.ac.uk>
2018-05-16 22:09:00 +0100

    Added style for the table.

--- server/src/Main.java
+++ server/src/Main.java
@@ -1,12 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-
-        DBConnecter connecter = new DBConnecter();
-        connecter.connect();
-        connecter.insert("HEi");
-        Data data = new Data();
+//        DBConnecter connecter = new DBConnecter();
+//        connecter.connect();
+//        connecter.insert("HEi");
         Server server = new Server();
         server.run();
     }
